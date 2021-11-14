f237d87d56d739b8ed5cd187af1153b21a2697ec
GuilhermeTeixeira13 <guilherme.teixeira@ubi.pt>
2021-11-14 09:48:09 +0000

    Added myinputs library

--- main/src/App.java
+++ main/src/App.java
@@ -1,5 +1,7 @@
+import myinputs.Ler;
+
 public class App {
     public static void main(String[] args){
-        
+        // Podemos usar as funções do Ler(int, string etc), já as implementei nas libraries
     }
 }

bfd5248eeb6f25ae52adeb13b0bb1e1b66a52870
João Baltazar <joaobaltazar55@gmail.com>
2021-11-15 11:48:46 +0000

    testeinicialMain
    Teste inicial para ver se está tudo bem.

--- main/src/App.java
+++ main/src/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args){
         // Podemos usar as funções do Ler(int, string etc), já as implementei nas libraries
         String s = Ler.umaString();
+        System.out.println(s);
     }
 }

cbe3ae7f48471e142b862b54bf636cf88c531b15
João Baltazar <joaobaltazar55@gmail.com>
2021-11-15 08:16:36 +0000

    Update App.java

--- main/src/App.java
+++ main/src/App.java
@@ -1,7 +1,17 @@
-import myinputs.Ler;
+
+
+import java.util.ArrayList;
+
+import javax.sound.sampled.SourceDataLine;
+
+
 
 public class App {
     public static void main(String[] args){
-        // Podemos usar as funções do Ler(int, string etc), já as implementei nas libraries
+        ArrayList<String> testestring = new ArrayList<>();
+        testestring.add("p");
+        testestring.add("o");
+        testestring.add("o");
+        System.out.println(testestring);
     }
 }

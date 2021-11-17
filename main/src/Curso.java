b9ce0c480484356b76db784fb9dbf6f2313fedcc
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2021-11-17 08:51:22 +0000

    adicionei classes e defini atributos da classe Curso
    Adicionei algumas classes usadas na Classe Curso (encontram-se vazias) e adicionei os atributos da classe Curso.

---
+++ main/src/Curso.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Curso {
+    String nomeCurso;
+    int codCurso;
+    ArrayList<Disciplina> disciplinasCurso;
+    ArrayList<Aluno> alunosCurso;
+    int duracaoEmHoras;
+    float mediaUltimoColocado;
+    Date dataInicio;
+    Date dataFim;
+
+}

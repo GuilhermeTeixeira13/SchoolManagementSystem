f82c6ece0a846d42375e12bfe4763ad5ea5afe0b
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2021-11-17 08:40:18 +0000

    limpeza na main

--- main/src/App.java
+++ main/src/App.java
@@ -1,9 +1,5 @@
-import myinputs.Ler;
-
 public class App {
     public static void main(String[] args){
-        // Podemos usar as funções do Ler(int, string etc), já as implementei nas libraries
-        String s = Ler.umaString();
-        System.out.println(s);
+        
     }
 }

bab4ff7bcd19b96898b8dff7e992bc215a4b7d31
klyn3 <claudioredondo.01@gmail.com>
2021-11-18 20:19:58 +0000

    updatefreq
    inicio

--- main/src/Frequencia.java
+++ main/src/Frequencia.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+import java.util.Date;
 public class Frequencia {
+    private int id;
+    private ArrayList<Disciplina> listdisc;
+    private Date datafreq;
+    private ArrayList<Professor> listprof;
+    private int numpergtotal;
+    private ArrayList<Perguntas> listperg;
+    private String dificuldade;
+    
+    public Frequencia(){
+        this.id = 0;
+        this.listdisc= new ArrayList<Disciplina>();
+        this.datafreq= new Date();
+        this.listprof= new ArrayList<Professor>();
+        this.numpergtotal=0;
+        this.listperg=new ArrayList<Perguntas>();
+        this.dificuldade="";
+    }
+
     
 }

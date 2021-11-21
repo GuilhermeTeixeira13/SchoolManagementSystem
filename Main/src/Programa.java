f7539ac8593cbc1775ed4679a55948441b7dd209
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2021-11-21 12:29:10 +0000

    Update Programa.java

--- Main/src/Programa.java
+++ Main/src/Programa.java
@@ -1,5 +1,11 @@
 public class Programa {
     public static void main(String[] args) {
-        System.out.println("teste");
+        int opcaoUtilizador;
+        System.out.println("Escola Professional de Informática");
+
+        do{
+            System.out.print("1 - Gerir Escola\n2 - Gerir Cursos\n3 - Gerir Disciplinas\n4 - Gerir Professores\n5 - Gerir Alunos\n6 - Gerir Professores");
+            opcaoUtilizador = Ler.umInt();
+        }while(opcaoUtilizador != 0 || opcaoUtilizador < 0 || opcaoUtilizador > 5);
     }
 }

e2e9d67937ca038a5ceb530d39f728dd67b57f78
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2021-11-21 12:30:49 +0000

    Update Programa.java

--- Main/src/Programa.java
+++ Main/src/Programa.java
@@ -4,7 +4,7 @@ public class Programa {
         System.out.println("Escola Professional de Informática");
 
         do{
-            System.out.print("1 - Gerir Escola\n2 - Gerir Cursos\n3 - Gerir Disciplinas\n4 - Gerir Professores\n5 - Gerir Alunos\n6 - Gerir Professores");
+            System.out.print("1 - Gerir Escola\n2 - Gerir Cursos\n3 - Gerir Disciplinas\n4 - Gerir Professores\n5 - Gerir Alunos\n6 - Gerir Professores\nESCOLHA A SUA OPCAO -> ");
             opcaoUtilizador = Ler.umInt();
         }while(opcaoUtilizador != 0 || opcaoUtilizador < 0 || opcaoUtilizador > 5);
     }

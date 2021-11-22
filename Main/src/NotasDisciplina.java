973bd045cfeeb4dc29cdfc882f9001d5af9bdf0d
SniperPRX <tigo.lopes.ribeiro@gmail.com>
2021-11-22 16:02:09 +0000

    Update NotasDisciplina.java
    Criei a classe NotasDisciplina

--- Main/src/NotasDisciplina.java
+++ Main/src/NotasDisciplina.java
@@ -1,3 +1,71 @@
+import java.util.ArrayList;
+
 public class NotasDisciplina {
+    // Atributos
+    private ArrayList<Resultado> notas;
+    private String disciplina;
+
+    // Contrutores
+    public NotasDisciplina(){
+        this.disciplina = "";
+        this.notas = new ArrayList<Resultado>();
+    }
+
+    public NotasDisciplina(String disciplina){
+        this.disciplina = disciplina;
+        this.notas = new ArrayList<Resultado>();
+    }
+
+    // Getters e Setters
+    public String getDisciplina(){
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina){
+        this.disciplina = disciplina;
+    }
+
+    public ArrayList<Resultado> getNotas(){
+        return notas;
+    }
+
+    public void setNotas(ArrayList<Resultado> notas){
+        this.notas = (ArrayList<Resultado>) notas.clone();
+    }
+
+    // toString
+    public String toString() {
+        String s;
+        s = "\nDisciplina: " + disciplina;
+        s = s + "Notas:\n";
+        for (int i = 0; i < notas.size(); i++)
+            s = s + notas.get(i) + "\n\n";
+        return s;
+    }
+
+    // Equals
+    public boolean equals(Object obj) {
+        boolean ig = false;
+        if (obj != null && this.getClass() == obj.getClass()) {
+            NotasDisciplina e = (NotasDisciplina) obj;
+            ig = (this.disciplina.equals(e.disciplina)) && (this.notas.equals(e.notas));
+        }
+        else {
+            ig = false;
+        }
+        return ig;
+    } 
     
+    // Clone
+    public Object clone() {
+        NotasDisciplina copia = new NotasDisciplina();
+        copia.disciplina = this.disciplina;
+        copia.notas = (ArrayList<Resultado>) this.notas.clone();
+        return copia;
+    }
+
+    public static void main(String[] args) {
+        // Testes
+    }
+
 }

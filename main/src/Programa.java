e97109d7ba6ac9cdee0404fc6defb5b9fec2ea3f
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2021-12-17 11:52:01 +0000

    Update Programa.java

--- main/src/Programa.java
+++ main/src/Programa.java
@@ -8,7 +8,6 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.lang.reflect.Array;
 import java.io.IOException;
 
 public class Programa implements Serializable {
